a661bbbb5e5ae83768a1f9a5cc2fa9f3632cbf58
Himat Singh <singhimat99@gmail.com>
2023-01-11 16:55:16 -0800

    minor styling changes

--- client/src/components/JobCard.tsx
+++ client/src/components/JobCard.tsx
@@ -29,21 +29,39 @@ export default function JobCard({ job, setJobs }: Props) {
     }
 
     return (
-        <div className="border border-black">
-            {job.title}{" "}
-            <button
-                onClick={() => {
-                    handleUpdateJob(job._id);
-                }}
-            >
-                Edit
-            </button>
-            <button
-                onClick={() => handleDeleteJob(job._id)}
-                className="text-red-500"
-            >
-                X
-            </button>
+        <div className="border border-black relative">
+            <div>
+                <h4>01/10/23</h4>
+                <h2>Company Name</h2>
+                <h4>
+                    <span>{job.title}</span>
+                    <span> · </span>
+                    <span>Fremont</span>
+                </h4>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Laboriosam quia totam repudiandae maiores repellat dolorum
+                    harum voluptatem quae recusandae. Laboriosam omnis quo ipsa
+                    nemo inventore illum aperiam explicabo exercitationem
+                    voluptates.
+                </p>
+                <div>Status</div>
+            </div>
+            <div className="absolute top-2 right-2 flex gap-2">
+                <button
+                    onClick={() => {
+                        handleUpdateJob(job._id);
+                    }}
+                >
+                    Edit
+                </button>
+                <button
+                    onClick={() => handleDeleteJob(job._id)}
+                    className="text-red-500"
+                >
+                    X
+                </button>
+            </div>
         </div>
     );
 }
